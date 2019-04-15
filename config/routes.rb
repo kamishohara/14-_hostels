019bad16fb13aba26edea8fa8f6379ed2a481b2f
kamishohara <kamishoharamasato@gmail.com>
2019-04-15 13:58:25 +0900

    3回目

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tweets
+  resources :users
   get 'tweets/new' => 'tweets#new'
   get 'tweets/index' => 'tweets#index'
   post 'tweets/new' => 'tweets#create'
